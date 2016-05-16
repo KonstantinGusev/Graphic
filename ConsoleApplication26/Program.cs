0bed5e2771f8b3f8614588434a83987356f13557
KonstantinGusev <konstantinvgusev@gmail.com>
2016-05-16 16:14:27 +0300

    комментирование строк кода

--- ConsoleApplication26/Program.cs
+++ ConsoleApplication26/Program.cs
@@ -16,29 +16,37 @@ namespace ConsoleApplication134
             
             var shapes = new List<Shape>();
 
-           
-
-                shapes.Add(new Star(5, 5, ConsoleColor.White));
-                
+                shapes.Add(new Star(5, 5, ConsoleColor.White));            
                 shapes.Add(new AnyStar(5, 5, ConsoleColor.Cyan, ConsoleColor.Gray));
-          
+
             while (true)
-                {
+            {
 
                 Engine.Draw(shapes);
-              
             }
-            
+              
         }
     }
 
+    /// <summary>
+    /// Класс объединяющий методы в которых картинки для анимации, а также иные методы для работы с ними
+    /// </summary>
     static class Engine
     {
+        /// <summary>
+        /// Метод очищает консоль
+        /// </summary>
         public static void Clean()
         {
             Console.Clear();
         }
 
+        /// <summary>
+        /// Первая картинка
+        /// </summary>
+        /// <param name="x">координаты x</param>
+        /// <param name="y">координаты y</param>
+        /// <param name="color">Цвет</param>
         public static void SetPixel(int x, int y, ConsoleColor color = ConsoleColor.DarkBlue)
         {
             Console.SetCursorPosition(x, y);
@@ -66,6 +74,13 @@ namespace ConsoleApplication134
             ████████████████████████████████████║░░░░░░░░█████░░░░░░░░║
         ");
         }
+
+        /// <summary>
+        /// Вторая картинка для анимации
+        /// </summary>
+        /// <param name="x">координаты х</param>
+        /// <param name="y">координаты у</param>
+        /// <param name="color">цвет</param>
         public static void SetPixel2(int x, int y, ConsoleColor color = ConsoleColor.DarkBlue)
         {
             Console.SetCursorPosition(x, y);
@@ -95,6 +110,10 @@ namespace ConsoleApplication134
         ");
         }
 
+        /// <summary>
+        /// метод очищает консоль а также переносит символы в массив List<> имеет таймер
+        /// </summary>
+        /// <param name="shapes"></param>
         public static void Draw(List<Shape> shapes)
         {
             Engine.Clean();
@@ -107,6 +126,9 @@ namespace ConsoleApplication134
         }
     }
 
+    /// <summary>
+    /// абстрактный класс передающий x и у
+    /// </summary>
     abstract class Shape
     {
         public abstract void Draw();
@@ -122,6 +144,9 @@ namespace ConsoleApplication134
         }
     }
 
+    /// <summary>
+    /// Класс 
+    /// </summary>
     class Star : Shape
     {
         public Star(int x, int y, ConsoleColor color)
@@ -135,20 +160,27 @@ namespace ConsoleApplication134
         public override void Draw()
         {
 
-
                 Engine.SetPixel(X , Y, this.Color);
                 Engine.SetPixel(X, Y , this.Color);
                 Engine.SetPixel(X,  Y, this.Color);
 
             Thread.Sleep(500);
                 Engine.Clean();
-            
-            
         }
     }
 
+    /// <summary>
+    /// Класс для второго метода реализации картинок
+    /// </summary>
     class AnyStar : Star
     {
+        /// <summary>
+        /// Метод принимающий 2 цвета 
+        /// </summary>
+        /// <param name="x">координата х</param>
+        /// <param name="y">координата н</param>
+        /// <param name="color1">цвет1</param>
+        /// <param name="color2">цвет2</param>
         public AnyStar(int x, int y, ConsoleColor color1, ConsoleColor color2)
             : base(x, y, color1)
         {
@@ -156,6 +188,9 @@ namespace ConsoleApplication134
             this.color2 = color2;
         }
 
+        /// <summary>
+        /// Реализация абстрактного класса измененный для перемены цвета а также возможно движение объекта
+        /// </summary>
         public override void Draw()
         {
             this.Color = (this.i++) % 2 == 0 ? this.color1 : this.color2;
